8875e156b298422102c5ab5e05fa4971427f3307
Adheesh Kadiresan <adheeshkadiresan@mba-2.local>
2024-10-27 15:39:34 -0400

    Created React project under client/

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Task from "./components/Task"
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello world again
+      <Task />
+      <Task />
+      <Task />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Task.js
@@ -0,0 +1,5 @@
+export default function Task() {
+    return (
+        <h1>This is a task.</h1>
+    )
+}
